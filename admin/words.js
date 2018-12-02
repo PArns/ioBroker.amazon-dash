a98a167d8e385154cb912e3720d44d3c04afa4ab
Moritz Heusinger <moritz.heusinger@gmail.com>
2018-12-02 13:13:35 +0100

    restore translation of adapter settings
    -in case someone still uses admin v2

--- admin/words.js
+++ admin/words.js
@@ -1,10 +1,19 @@
 systemDictionary = {
+    "settings": {
+        "en": "Amazon Dash Adapter settings",
+        "de": "Amazon Dash Adapter-Einstellungen",
+        "ru": "Настройки драйвера"
+    },
     "pcap": {
         "en": "PCAP Interface (leave empty for default)",
         "de": "PCAP Interface (lasse leer wenn default)",
         "ru": "Интерфейс PCAP (оставить пустыми по умолчанию)"
     },
     "name": {"en": "Name", "de": "Name", "ru": "Имя"},
-    "mac": {"en": "MAC Address format first xx:xx:xx", "de": "MAC Adresse die ersten xx:xx:xx", "ru": "MAC Адрес xx:xx:xx"},
+    "mac": {
+        "en": "MAC Address format first xx:xx:xx",
+        "de": "MAC Adresse die ersten xx:xx:xx",
+        "ru": "MAC Адрес xx:xx:xx"
+    },
 
 };
